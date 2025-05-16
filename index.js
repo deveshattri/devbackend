6fc160f0f9985811a56ef2012a1655986618d6f4
deveshattri <deveshattrikansera9568@gmail.com>
2025-05-16 19:00:52 +0530

    new changes

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.get('/', (req, res) => {
 app.get('/dev',(req,res) => {
     res.send('<h1>yes devesh</h1>')
 })
+const jsondata={"name": "John Doe", "age": 30, "city": "New York"}
+
+app.get('/github',(req,res)=>{
+    res.json(jsondata)
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
